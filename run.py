4f8cc7a030dcfff7f62f5161aba6eb43fcb1a27d
Tbug-debug <76459231+Tbug-debug@users.noreply.github.com>
2023-01-10 19:28:24 +0900

    추가| repository & setting

--- run.py
+++ run.py
@@ -29,9 +29,11 @@ def idol_post():
     db.idol.insert_one(doc)
     return jsonify({'msg': '응원 완료!'})
 
+
 @app.route("/idol", methods=["GET"])
 def idol_get():
-    return render_template('test.html')
+    idol_list = list(db.idol.find({},{'_id':False}))
+    return jsonify({'idol':idol_list})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
