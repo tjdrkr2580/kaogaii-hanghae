dd1d40712ba79addd1da1728f6e11e17e960362d
Tbug-debug <76459231+Tbug-debug@users.noreply.github.com>
2023-01-10 17:09:20 +0900

    추가 | repository & setting

---
+++ run.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, jsonify
+
+from pymongo import MongoClient
+
+client = MongoClient('mongodb+srv://admin:01234@sparta.sjbnpdb.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route("/idol", methods=["POST"])
+def idol_post():
+    # db.idol.insert_one(doc)
+    
+    count = len(list(db.idol.find({}))) + 1
+    idol_name = request.form['idol_name']
+    idol_link = request.form['idol_link']
+
+    doc = {
+        'idol_num' : count,
+        'idol_name' : idol_name,
+        'idol_link' : idol_link
+    }
+
+    db.idol.insert_one(doc)
+    return jsonify({'msg': '응원 완료!'})
+
+@app.route("/idol", methods=["GET"])
+def idol_get():
+    return render_template('test.html')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
