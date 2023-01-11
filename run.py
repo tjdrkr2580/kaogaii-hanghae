ded756c84b897d9e4039b3428bb04ba0350e16fd
Tbug-debug <76459231+Tbug-debug@users.noreply.github.com>
2023-01-11 10:49:27 +0900

    백엔드수정| repository & setting

--- run.py
+++ run.py
@@ -38,12 +38,6 @@ def idol_get():
     idol_list = list(db.idol.find({},{'_id':False}))
     return jsonify({'idol':idol_list})
 
-@app.route('/fanpage', methods=["GET"])
-def fanpage_go():
-    #jk fanpage go func
-    user_list = list(db.idol.find({},{'_id':False}))
-    return jsonify({'kdoe':user_list})
-
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
